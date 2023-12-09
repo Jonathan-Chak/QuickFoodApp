0247fda7ef33f4ace39f888e5150396c55811dd5
Jonathan-Chak <133053615+Jonathan-Chak@users.noreply.github.com>
2023-12-09 13:56:56 -0500

    expanded menu

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -5,6 +5,30 @@ export const ITEMS: Item[] = [
     {id: 1, restaurant: "Papa Johns", name: "Hawaiian Pizza", price: 20 },
     {id: 2, restaurant: "Subway", name: "Bacon Sandwich", price: 15 },
     {id: 3, restaurant: "Tim Hortons", name: "Espresso", price: 9 },
-    {id: 4, restaurant: "Ruby Wednesday", name: "Satisfying Salad", price: 9 },
-    {id: 5, restaurant: "KFC", name: "Spicy Fried Chicken", price: 12 }
+    {id: 4, restaurant: "Ruby Tuesday", name: "Satisfying Salad", price: 9 },
+    {id: 5, restaurant: "KFC", name: "Spicy Fried Chicken", price: 12 },
+    {id: 6, restaurant: "KFC", name: "FESTIVE CHICKEN SANDWICH BOX MEAL", price: 32 },
+    {id: 7, restaurant: "KFC", name: "FESTIVE CHICKEN SANDWICH COMBO", price: 11.99 },
+    {id: 8, restaurant: "KFC", name: "FESTIVE CHICKEN SANDWICH", price: 7 },
+    {id: 9, restaurant: "KFC", name: "10 PIECE FESTIVE FAVOURITES FEAST", price: 42 },
+    {id: 10, restaurant: "KFC", name: "6 PIECE BUCKET AND 2 LARGE SIDES", price: 22 },
+    {id: 11, restaurant: "KFC", name: "18 PIECE BUCKET AND 5 LARGE SIDES", price: 32 },
+    {id: 12, restaurant: "KFC", name: "POUTINE", price: 10 },
+    {id: 13, restaurant: "KFC", name: "PEPSI", price: 0 },
+    {id: 14, restaurant: "Tim Hortons", name: "Everything Bagel", price: 2.19 },
+    {id: 15, restaurant: "Tim Hortons", name: "Four Cheese Bagel", price: 2.79 },
+    {id: 16, restaurant: "Tim Hortons", name: "6 Assorted Bagels", price: 9.99 },
+    {id: 17, restaurant: "Tim Hortons", name: "Sour Cream Glazed Donut", price: 9.99 },
+    {id: 18, restaurant: "Papa Johns", name: "Original Crust", price: 15 },
+    {id: 19, restaurant: "Papa Johns", name: "Epic Stuffed Crust", price: 16 },
+    {id: 20, restaurant: "Papa Johns", name: "Epic Pepperoni-Stuffed Crust", price: 18 },
+    {id: 21, restaurant: "Ruby Tuesday", name: "SNOW CRAB DINNER", price: 20 },
+    {id: 22, restaurant: "Ruby Tuesday", name: "SCALLOPS DINNER", price: 22 },
+    {id: 23, restaurant: "Ruby Tuesday", name: "T-BONE STEAK", price: 0.99 },
+    {id: 24, restaurant: "Ruby Tuesday", name: "STUFFED MUSHROOMS APPETIZER", price: 15 },
+    {id: 25, restaurant: "Ruby Tuesday", name: "LOADED POTATO SKINS", price: 15 },
+    {id: 26, restaurant: "Ruby Tuesday", name: "RIB EYE*, RIBS, AND CRISPY SHRIMP COMBO", price: 25 }
+    
+    
+
 ]
